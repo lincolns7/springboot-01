49973934d72c0a9922ca9761470bc5753b892d13
lcn 777 <scherrer.lincoln09@gmail.com>
2026-04-08 17:32:46 -0300

    Spring boot

---
+++ src/main/java/com/BID_da_varzea/main/controller/PortalController.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.BID_da_varzea.main.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ *
+ * @author Aluno
+ */
+@Controller
+public class PortalController {
+    @GetMapping("/portal")
+    public String abrirPortal() {
+        return "portal";
+    }
+}
---
+++ src/main/java/com/BID_da_varzea/main/controller/UsuarioController.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.BID_da_varzea.main.controller;
+
+/**
+ *
+ * @author Aluno
+ */
+public class UsuarioController {
+    
+}
